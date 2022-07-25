efff1edee88873da1212427ddbbc37fdd01c9de4
VM_Ubuntu1 <g1131786239@gmail.com>
2022-07-25 20:15:56 +0300

    fix dist folder path

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
